9c8f370d434696095195f9de7b8a61bbeff1f58d
Omar <rajamuhammedomar@gmail.com>
2023-08-23 00:59:00 +0500

    Complete the calculator :)

--- script.js
+++ script.js
@@ -1,53 +1,116 @@
-const btns = document.querySelectorAll('.operators>button')
+const numbers = document.querySelectorAll('.num')
 const screen = document.querySelector('.screen')
 const clearBtn = document.querySelector('#clear')
 const deleteBtn = document.querySelector("#delete")
-let values = ''
+const operators = document.querySelectorAll(".op")
+const eq = document.querySelector(".eq")
+const decimal = document.querySelector('.decimal')
 
-// number buttons
-btns.forEach((btn) => {
-    btn.addEventListener('click', () => {
-        if (btn.textContent != 'Delete' && btn.textContent != 'Clear') {
-            if ((!isNaN(btn.textContent) || (!isNaN(values[values.length - 1])))) {
-                values += btn.textContent
-            }
-            else {
-                values = values.slice(0, -1) + btn.textContent
-            }
-            console.log(values)
+const oldScreen = document.querySelector(".old-screen")
+const newScreen = document.querySelector(".new-screen")
+
+let curr = ""
+let prev = ""
+let operator = ""
+
+// numbers
+numbers.forEach(btn => {
+    btn.addEventListener('click', (event) => {
+        if (newScreen.textContent == 'Error') {
+            newScreen.textContent = ''
         }
-    });
-});
-
-
-
-
-
-// clear button 
-clearBtn.addEventListener('click', () => {
-    values = ''
-    screen.textContent = ''
+        curr += event.target.textContent
+        newScreen.textContent = curr
+    })
 })
+// operators
+operators.forEach(btn => {
+    // to eval expressions without presing equals
+    btn.addEventListener('click', (event) => {
+        if (curr && prev) {
+            evaluate()
+        }
+        if (curr) {
+            // change values
+            prev = curr
+            curr = ""
+            operator = event.target.textContent
+            // set screens
+            newScreen.textContent = ''
+            oldScreen.textContent = prev + ' ' + operator
+        }
+    })
+})
+
+// decimal
+decimal.addEventListener('click', () => {
+    if (!curr.includes('.')) {
+        curr += '.'
+        newScreen.textContent = curr
+    }
+})
+
+// clear screen and stored values
+const clear = () => {
+    prev = ''
+    curr = ''
+    operator = ''
+    newScreen.textContent = ''
+    oldScreen.textContent = ''
+}
+// clear button 
+clearBtn.addEventListener('click', clear)
 
 // delete button
 deleteBtn.addEventListener('click', () => {
-    values = values.slice(0, -1)
-    screen.textContent = values
+    curr = curr.slice(0, -1)
+    newScreen.textContent = curr
 
 })
 
-const operate = (num1, operator, num2) => {
-    switch (operator) {
+// equals button
+eq.addEventListener('click', () => {
+    if (prev && curr) {
+        evaluate()
+    }
+})
+
+
+// update values on screens
+const evaluate = () => {
+    curr = operate()
+    curr = curr.toString()
+    newScreen.textContent = curr
+    oldScreen.textContent = ''
+
+}
+
+// solve expressions
+const operate = () => {
+    prev = Number(prev)
+    curr = Number(curr)
+    op = operator
+    switch (op) {
         case '+':
-            return add(num1, num2)
+            return add(prev, curr)
+
         case '-':
-            return subtract(num1, num2)
-        case 'x':
-            return multiply(num1, num2)
+            return subtract(prev, curr)
+
+        case '*':
+            return multiply(prev, curr)
+
         case '/':
-            return divide(num1, num2)
+            if (curr <= 0) {
+                clear()
+                newScreen.textContent = 'Error'
+                return
+            }
+            return divide(prev, curr)
     }
 }
+
+
 
 const add = function (a, b) {
     return a + b
@@ -61,10 +124,9 @@ const sum = function (arr) {
     return arr.reduce((acc, elem) => acc + elem, 0)
 };
 
-const multiply = function (arr, num2) {
-    return Array.isArray(arr) ? arr.reduce((acc, elem) => acc * elem, 1) : arr * num2
+const multiply = function (arr, curr) {
+    return Array.isArray(arr) ? arr.reduce((acc, elem) => acc * elem, 1) : arr * curr
 };
-
 
 const divide = function (a, b) {
     return a / b
