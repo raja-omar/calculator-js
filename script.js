a8d00cec453a6d7261391dd38040247040290d8e
Omar <rajamuhammedomar@gmail.com>
2023-08-22 05:53:35 +0500

    Print the current stored string properly on screen

--- script.js
+++ script.js
@@ -1,17 +1,51 @@
-const btns = document.querySelectorAll('button')
+const btns = document.querySelectorAll('.operators>button')
 const screen = document.querySelector('.screen')
+const clearBtn = document.querySelector('#clear')
+const deleteBtn = document.querySelector("#delete")
+let values = ''
 
+// number buttons
+btns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        if (btn.textContent != 'Delete' && btn.textContent != 'Clear') {
+            if ((!isNaN(btn.textContent) || (!isNaN(values[values.length - 1])))) {
+                values += btn.textContent
+            }
+            else {
+                values = values.slice(0, -1) + btn.textContent
+            }
+            console.log(values)
+        }
+    });
+});
+
+
+
+
+
+// clear button 
+clearBtn.addEventListener('click', () => {
+    values = ''
+    screen.textContent = ''
+})
+
+// delete button
+deleteBtn.addEventListener('click', () => {
+    values = values.slice(0, -1)
+    screen.textContent = values
+
+})
 
 const operate = (num1, operator, num2) => {
     switch (operator) {
         case '+':
             return add(num1, num2)
         case '-':
-           return subtract(num1, num2)
+            return subtract(num1, num2)
         case 'x':
-            return multiply(num1,num2)
+            return multiply(num1, num2)
         case '/':
-            return divide(num1,num2)
+            return divide(num1, num2)
     }
 }
 
@@ -33,5 +67,5 @@ const multiply = function (arr, num2) {
 
 
 const divide = function (a, b) {
-    return a/b
+    return a / b
 }
